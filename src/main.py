32d2a8e92828dc31507a2201eab05883db17740d
Marco Bonifacio <bonifacio.marco@gmail.com>
2023-04-06 18:09:31 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,31 +11,18 @@ def f(n, d, m, t):
     return n / d + n ** 2 / m + n ** 3 / y
 
 def p(giorno, mese, anno, N, step):
-    z = np.array([np.exp(2 * np.pi * 1j * f(n, giorno, mese, anno)) for n in range(3, N * 3, step)])
+    z = np.array([np.exp(2 * np.pi * 1j * f(n, giorno, mese, anno)) 
+        for n in range(3, N * 3, step)])
     z = z.cumsum()
     df = pd.DataFrame([z.real, z.imag]).T.reset_index()
     df.columns = ['sort', 'real', 'imag']
     alt.data_transformers.enable('json')
-    chart = alt.Chart(df).mark_line().encode(
-        x=alt.X('real', axis=alt.Axis(title='')),
-        y=alt.Y('imag', axis=alt.Axis(title='')),
-        order='sort'
-    ).properties(
-        title='{}/{}/{}'.format(giorno, mese, anno)
-    ).configure_title(
-        font='calibri',
-        fontSize=16,
-        fontWeight='bold'
-    ).configure_axis(
-        grid=False, 
-        domain=False, 
-        labels=False, 
-        ticks=False
-    ).configure_view(
-        strokeOpacity=0,
-        height=350,
-        width=350
-    )
-    return chart
+    fig, ax = plt.subplots(figsize=(4, 4))
+    ax.plot(z.real, z.imag, color='#4c78a8')
+    ax.set_aspect(1)
+    plt.axis('off')
+    ax.set_title('{}/{}/{}'.format(giorno, mese, anno), 
+        {'fontname': 'calibri', 'fontsize': 16, 'fontweight': 'bold'})
+    return fig
 
 display(p(d, m, y, 10000, 1))
