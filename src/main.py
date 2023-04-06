9244ca99299216298fbb481999c608113aa8f604
bonifacio.marco@gmail.com <bonifacio.marco@gmail.com>
2023-04-07 00:44:01 +0200

    Several optimizations

--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@ import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
 
+import js
+
 d = dt.datetime.today().day
 m = dt.datetime.today().month
 y = dt.datetime.today().year
+
+div_date = js.document.getElementById('div-date')
 
 def f(n, d, m, t):
     return n / d + n ** 2 / m + n ** 3 / y
@@ -17,8 +21,7 @@ def p(giorno, mese, anno, N, step):
     ax.plot(z.real, z.imag, color='#4682b4')
     ax.set_aspect(1)
     plt.axis('off')
-    ax.set_title('{}/{}/{}'.format(giorno, mese, anno), 
-        {'fontname': 'calibri', 'fontsize': 16, 'fontweight': 'bold'})
     return fig
 
+div_date.innerHTML = f'{d:02d}-{m:02d}-{y}'
 display(p(d, m, y, 10000, 1), target='canvas')
