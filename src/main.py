2ff64f832831467763dc791c9420275bc489347e
Marco Bonifacio <bonifacio.marco@gmail.com>
2023-04-06 17:49:37 +0200

    Switch to altair

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
+import altair as alt
 
 d = dt.datetime.today().day
 m = dt.datetime.today().month
@@ -12,11 +13,27 @@ def f(n, d, m, t):
 def p(giorno, mese, anno, N, step):
     z = np.array([np.exp(2 * np.pi * 1j * f(n, giorno, mese, anno)) for n in range(3, N * 3, step)])
     z = z.cumsum()
-    fig, ax = plt.subplots(figsize=(6, 6))
-    ax.plot(z.real, z.imag, color='#4c78a8')
-    ax.set_aspect('equal')
-    ax.axis('off')
-    ax.set_title(f'{giorno:02d}-{mese:02d}-{anno}')
-    return fig
+    df = pd.DataFrame([z.real, z.imag]).T.reset_index()
+    df.columns = ['sort', 'real', 'imag']
+    alt.data_transformers.enable('json')
+    chart = alt.Chart(df).mark_line().encode(
+        x=alt.X('real', axis=alt.Axis(title='')),
+        y=alt.Y('imag', axis=alt.Axis(title='')),
+        order='sort'
+    ).properties(
+        title='{}/{}/{}'.format(giorno, mese, anno)
+    ).configure(
+        title=alt.VgTitleConfig(font='calibri', fontSize=16, fontWeight='bold')
+    ).configure_axis(
+        grid=False, 
+        domain=False, 
+        labels=False, 
+        ticks=False,
+    ).configure_view(
+        strokeOpacity=0,
+        height=350,
+        width=350
+    )
+    return chart
 
 display(p(d, m, y, 10000, 1))
