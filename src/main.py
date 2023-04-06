bebcdead19a9967beeb5dccbcbb02d3014b905ba
bonifacio.marco@gmail.com <bonifacio.marco@gmail.com>
2023-04-06 19:10:10 +0200

    Scaffold page

--- src/main.py
+++ src/main.py
@@ -14,11 +14,11 @@ def p(giorno, mese, anno, N, step):
         for n in range(3, N * 3, step)])
     z = z.cumsum()
     fig, ax = plt.subplots(figsize=(4, 4))
-    ax.plot(z.real, z.imag, color='#4c78a8')
+    ax.plot(z.real, z.imag, color='#4682b4')
     ax.set_aspect(1)
     plt.axis('off')
     ax.set_title('{}/{}/{}'.format(giorno, mese, anno), 
         {'fontname': 'calibri', 'fontsize': 16, 'fontweight': 'bold'})
     return fig
 
-display(p(d, m, y, 10000, 1))
+display(p(d, m, y, 10000, 1), target='canvas')
